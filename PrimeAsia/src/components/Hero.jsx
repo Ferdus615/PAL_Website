c46bf8693dff4e99809a5125781c8fa09d96ef66
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2024-12-29 23:47:56 +0600

    Hero start

---
+++ PrimeAsia/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div>
+      <div className="hero">
+        <img src="./images/cover photo.jpg" alt="Cover Photo" />
+        <div className="overlay"></div>
+        <div className="hero-text">
+          <p className="title">Prime Automations Ltd.</p>
+          <p className="slogan">Sustainability Through Technology</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
