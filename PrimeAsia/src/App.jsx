a313ac0cd60a6ed359cf6ce2ee297b5373118283
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2024-12-29 22:34:15 +0600

    Nav Done

--- PrimeAsia/src/App.jsx
+++ PrimeAsia/src/App.jsx
@@ -1,20 +1,10 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+
 const App = () => {
   return (
     <div>
-      <nav>
-        <div className="nav_logo">
-          <img src="pal logo nav.svg" alt="PAL logo" />
-        </div>
-        <div className="nva-links">
-          <ul>
-            <li href="#">Products</li>
-            <li href="#">About</li>
-            <li href="#">News</li>
-            <li href="#">Blogs</li>
-            <li href="#">Contact</li>
-          </ul>
-        </div>
-      </nav>
+      <Navigation />
     </div>
   );
 };
---
+++ PrimeAsia/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="nav_logo">
+        <img href="#" src="pal logo nav.svg" alt="PAL logo" />
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li href="#">Products</li>
+          <li href="#">About</li>
+          <li href="#">News</li>
+          <li href="#">Blogs</li>
+          <li href="#">Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
